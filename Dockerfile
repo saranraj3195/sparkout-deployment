87b2afa384004fdd60cc666074aabb02055b2384
saranraj3195 <saranraj.st0078@sparkouttech.com>
2024-05-08 12:05:08 +0530

    new update

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,7 @@ FROM ubuntu:bionic
 RUN apt update
 RUN apt -y install rsync openssh-client
 
-
-# Label
-LABEL "com.github.actions.name"="Deploy with rsync"
-LABEL "maintainer"="Obinna Odirionye <odirionye@mail.com>"
-
-
-ADD entrypoint.sh /entrypoint.sh
+COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Debugging: Print out variable values
+echo "DEPLOY_KEY: $DEPLOY_KEY"
+echo "USERNAME: $USERNAME"
+echo "SERVER_IP: $SERVER_IP"
+echo "SERVER_PORT: $SERVER_PORT"
+echo "SERVER_DESTINATION: $SERVER_DESTINATION"
+echo "ARGS: $ARGS"
+echo "FOLDER: $FOLDER"
+echo "GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
+
 SSHPATH="$HOME/.ssh"
 
 mkdir -p "$SSHPATH"
@@ -10,5 +20,8 @@ chmod 600 "$SSHPATH/key"
 
 SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
 
-# sync it up"
+# Debugging: Print out rsync command
+echo "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
+
+# Sync files using rsync
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
