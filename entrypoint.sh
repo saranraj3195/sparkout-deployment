f763ca5228c719cfebbfa0b446d9e45a57846737
saranraj9531 <102714548+saranraj3195@users.noreply.github.com>
2024-05-09 16:31:44 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -eu
 
-# Retrieve secrets from GitHub Actions
 SSHPATH="$HOME/.ssh"
+if [ ! -d "$SSHPATH" ]; then
+  mkdir -p "$SSHPATH"
+fi
+
 echo "$DEPLOY_KEY" > "$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
 
-# Set up deployment server details using environment variables
 SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
-
-# Define rsync options and execute the deployment
-RSYNC_OPTIONS="-avz --delete"
-sh -c "rsync $RSYNC_OPTIONS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
+# Sync files using rsync
+sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
